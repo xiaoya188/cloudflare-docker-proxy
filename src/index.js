977a2f4e17e1bd75d8978d3ab1e042256ff8a8ee
xiaoya188 <xijin286@gmail.com>
2024-08-07 05:38:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "cc.fafe.us.kg": dockerHub,
+  "quay.fafe.us.kg": "https://quay.io",
+  "gcr.fafe.us.kg": "https://gcr.io",
+  "k8s-gcr.fafe.us.kg": "https://k8s.gcr.io",
+  "k8s.fafe.us.kg": "https://registry.k8s.io",
+  "ghcr.fafe.us.kg": "https://ghcr.io",
+  "cloudsmith.fafe.us.kg": "https://docker.cloudsmith.io",
+  "ecr.fafe.us.kg": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.fafe.us.kg": dockerHub,
 };
 
 function routeByHosts(host) {
